18efe5b3b6c9e5f8478b9fc1b04b09ca784bec49
esteban <esteb.garnil@gmail.com>
2023-04-09 17:33:46 +0200

    oui

--- it.py
+++ it.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import math
 
+
 wiki = wikipediaapi.Wikipedia('fr')
 
 tk = Tk()
@@ -15,6 +16,10 @@ class bulle:
         self.val = val
         self.x = pos[0]
         self.y = pos[1]
+    
+    def affichage_noeud(self):
+        global canvas
+        self.affichage = canvas.create_oval(self.x, self.y, self.x+20, self.y+20,fill='red')
 
 class noeud(bulle):
     def __init__(self, val, voisins, pos=(0,0)):
@@ -22,9 +27,7 @@ class noeud(bulle):
         self.voisins = voisins
         self.affichage = None
 
-    def affichage_noeud(self):
-        global canvas
-        self.affichage = canvas.create_oval(self.x, self.y, self.x+20, self.x+20,fill='red')
+
 
 def recuperation_page(page, nbr=40):
     if type(page) == type('str'): 
@@ -37,40 +40,52 @@ def recuperation_page(page, nbr=40):
     return noeud(page.title, voisins)
 
 
+def nbr_cercle(lst):
+    lst_remplace = []
+    i = 0
+    while len(lst)> 0:
+        i += 1
+        lst_remplace.append([lst.pop(0) for el in lst[:i*8]])
+    return lst_remplace
+
+
 
 def graphe(obj):
-    position = [50,50]
+    position = [300,300]
     obj.x, obj.y = position[0], position[1]
-    if len(obj.voisins)<8:
-        lst_angle = [el*(360 / len(obj.voisins)) for el in range(len(obj.voisins))]
-    for i in range(len(obj.voisins)):
-        obj.voisins[i].x = obj.x + 8 * math.cos(lst_angle[i]*math.pi / 180)
-        obj.voisins[i].y + 8 * math.sin(lst_angle[i]*math.pi / 180)
+
+
+    print(len(obj.voisins))
+    lst_cercle = nbr_cercle(obj.voisins)
+    for i in range(len(lst_cercle)):
+        print(len(lst_cercle[i]))
+        for j in range(len(lst_cercle[i])):
+            lst_angle = [el*(360 / len(lst_cercle[i])) for el in range(len(lst_cercle[i]))]
+            lst_cercle[i][j].x = obj.x + 50*(1+i) * math.cos(lst_angle[j]*math.pi / 180)
+            lst_cercle[i][j].y = obj.y + 50*(1+i) * math.sin(lst_angle[j]*math.pi / 180)
+            lst_cercle[i][j].affichage_noeud()
+    obj.affichage_noeud()
+    
 
 
 
-# graphe(recuperation_page('Catégorie:Histoire'))
-
-
-
-
-
-
-
-
-
-# #Creation  d'un bouton "Quitter":
-# Bouton_Quitter=Button(tk, text ='Quitter', command = tk.destroy)
-# #On ajoute l'affichage du bouton dans la fenêtre tk:
-# Bouton_Quitter.pack()
-
-# balle = noeud('a', [], (40,40))
-# balle.affichage_noeud()
+graphe(recuperation_page('Catégorie:Histoire'))
 
 
 
 
-# tk.mainloop()
+
+
+
+
+
+#Creation  d'un bouton "Quitter":
+Bouton_Quitter=Button(tk, text ='Quitter', command = tk.destroy)
+#On ajoute l'affichage du bouton dans la fenêtre tk:
+Bouton_Quitter.pack()
+
+
+tk.mainloop()
 
 
 
