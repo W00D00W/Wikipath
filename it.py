ed1676b1b713c9d5aca164ef5b7c8116fdba7c13
esteban <esteb.garnil@gmail.com>
2023-04-09 16:29:44 +0200

    change

--- it.py
+++ it.py
@@ -10,12 +10,16 @@ tk = Tk()
 canvas = Canvas(tk,width = 1000, height = 1000 , bd=0, bg="white")
 canvas.pack(padx=10,pady=10)
 
-class noeud():
-    def __init__(self, val, voisins, pos=(0,0)):
+class bulle:
+    def __init__(self, val, pos = (0,0)):
         self.val = val
-        self.voisins = voisins
         self.x = pos[0]
         self.y = pos[1]
+
+class noeud(bulle):
+    def __init__(self, val, voisins, pos=(0,0)):
+        super().__init__(val, pos)
+        self.voisins = voisins
         self.affichage = None
 
     def affichage_noeud(self):
@@ -26,29 +30,23 @@ def recuperation_page(page, nbr=40):
     if type(page) == type('str'): 
         page = wiki.page(page)
     if page.title.count('Catégorie:')>=1:
-            voisins = [el for el in wiki.page(page.title).categorymembers.values() if el.ns == 0 or el.ns == 14]
+            voisins = [bulle(el.title) for el in wiki.page(page.title).categorymembers.values() if el.ns == 0 or el.ns == 14]
     else:
-        voisins = [el for el in wiki.page(page.title).links.values() if el.ns == 0 or el.ns == 14] ## recupere une liste filtrée
+        voisins = [bulle(el.title) for el in wiki.page(page.title).links.values() if el.ns == 0 or el.ns == 14] ## recupere une liste filtrée
     voisins = voisins[:min(nbr, len(voisins))] ## réduit la liste au nombre d'élement demandé
     return noeud(page.title, voisins)
 
 
 
-def graphe(dico):
+def graphe(obj):
     position = [50,50]
-    val = [el.val for el in dico.values()]
-    print(val)
-    for val in dico.values():
-        val.x, val.y = position[0], position[1]
-        if len(val.voisins)<8:
-            angle = 360 / len(val.voisins)
-            lst_angle = [el*angle for el in range(len(val.voisins))]
-        for i in range(len(val.voisins)):
-            dico[val.voisins[i]].x = val.x + 8 * math.cos(lst_angle[i]*math.pi / 180)
-            dico[val.voisins[i]].y = val.y + 8 * math.sin(lst_angle[i]*math.pi / 180)
+    obj.x, obj.y = position[0], position[1]
+    if len(obj.voisins)<8:
+        lst_angle = [el*(360 / len(obj.voisins)) for el in range(len(obj.voisins))]
+    for i in range(len(obj.voisins)):
+        obj.voisins[i].x = obj.x + 8 * math.cos(lst_angle[i]*math.pi / 180)
+        obj.voisins[i].y + 8 * math.sin(lst_angle[i]*math.pi / 180)
 
-
-    
 
 
 # graphe(recuperation_page('Catégorie:Histoire'))
