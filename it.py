289b454a719df056a0421ad5fc880fb0e378936a
esteban <esteb.garnil@gmail.com>
2023-04-10 14:38:22 +0200

    Update it.py

--- it.py
+++ it.py
@@ -18,11 +18,17 @@ class bulle:
         self.x = pos[0]
         self.y = pos[1]
         self.affichage = None
+        self.affichage_texte = None
 
     
     def affichage_noeud(self):
         global canvas
-        self.affichage = canvas.create_oval(self.x, self.y, self.x+20, self.y+20,fill='red')
+        padding = 10 # espace de rembourrage entre le texte et le bord du rectangle
+        bbox = canvas.bbox(canvas.create_text(self.x, self.y, text=self.val)) # dimensions du rectangle englobant
+        bbox = (bbox[0]-padding, bbox[1]-padding, bbox[2]+padding, bbox[3]+padding) # ajout de rembourrage
+        radius = 20 # rayon des coins arrondis
+        self.affichage = create_rounded_rectangle(canvas, *bbox, radius=radius, fill="#3CA6A6", outline="#026773", width=2)
+        self.affichage_texte = canvas.create_text(self.x, self.y, text=self.val, width=bbox[2]-bbox[0]-2*padding, justify=CENTER, fill="#FFFFFF")
 
 
 class noeud(bulle):
@@ -31,7 +37,7 @@ class noeud(bulle):
         self.voisins = voisins
 
 
-def recuperation_page(page, nbr=40):
+def recuperation_page(page, nbr=48):
     if type(page) == type('str'): 
         page = wiki.page(page)
     if page.title.count('Catégorie:')>=1:
@@ -50,68 +56,55 @@ def nbr_cercle(lst):
         lst_remplace.append([lst.pop(0) for el in lst[:i*8]])
     return lst_remplace
 
-# # définition de la fonction pour créer un rectangle avec des bords arrondis
-# def create_rounded_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
-#     points = [x1+radius, y1,
-#               x1+radius, y1,
-#               x2-radius, y1,
-#               x2-radius, y1,
-#               x2, y1,
-#               x2, y1+radius,
-#               x2, y1+radius,
-#               x2, y2-radius,
-#               x2, y2-radius,
-#               x2, y2,
-#               x2-radius, y2,
-#               x2-radius, y2,
-#               x1+radius, y2,
-#               x1+radius, y2,
-#               x1, y2,
-#               x1, y2-radius,
-#               x1, y2-radius,
-#               x1, y1+radius,
-#               x1, y1+radius,
-#               x1, y1]
-#     return canvas.create_polygon(points, smooth=True, **kwargs)
+# définition de la fonction pour créer un rectangle avec des bords arrondis
+def create_rounded_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
+    points = [x1+radius, y1,
+              x1+radius, y1,
+              x2-radius, y1,
+              x2-radius, y1,
+              x2, y1,
+              x2, y1+radius,
+              x2, y1+radius,
+              x2, y2-radius,
+              x2, y2-radius,
+              x2, y2,
+              x2-radius, y2,
+              x2-radius, y2,
+              x1+radius, y2,
+              x1+radius, y2,
+              x1, y2,
+              x1, y2-radius,
+              x1, y2-radius,
+              x1, y1+radius,
+              x1, y1+radius,
+              x1, y1]
+    return canvas.create_polygon(points, smooth=True, **kwargs)
 
 ## fonction qui affiche le graphe
 def graphe(obj):
-    position = [300,300]
+    position = [400,400]
     obj.x, obj.y = position[0], position[1]
     lst_angle = [el*(360 / len(obj.voisins)) for el in range(len(obj.voisins))]
 
     lst_cercle = nbr_cercle(obj.voisins)
     lst_cercle = [[lst_cercle[el]]+[100*(1+el)] for el in range(len(lst_cercle))]
-    while len(lst_cercle) > 0:
-        j = -1
+    while len(lst_cercle[0][0]) > 0: ### max de rep possible 
         for i in range(len(lst_cercle)):
-            j += 1
-            obj.voisins.append(lst_cercle[j][0].pop(0))
-            obj.voisins[-1].x = obj.x + lst_cercle[j][1] * math.cos(lst_angle[0]*math.pi / 180)
-            obj.voisins[-1].y = obj.y + lst_cercle[j][1] * math.sin(lst_angle.pop(0)*math.pi / 180)
-            obj.voisins[-1].affichage_noeud()
-            if lst_cercle[j][0] == []:
-                lst_cercle.pop(j)
-                j -= 1
+            for j in range(1+i):
+                if lst_cercle[i][0] != []:
+                    obj.voisins.append(lst_cercle[i][0].pop(0))
+                    obj.voisins[-1].x = obj.x + lst_cercle[i][1] * math.cos(lst_angle[0]*math.pi / 180)
+                    obj.voisins[-1].y = obj.y + lst_cercle[i][1] * math.sin(lst_angle.pop(0)*math.pi / 180)
+                    obj.voisins[-1].affichage_noeud()
     obj.affichage_noeud()
 
             
 
-    # for i in range(len(lst_cercle)):
-    #     print(len(lst_cercle[i]))
-    #     for j in range(len(lst_cercle[i])):
-    #         lst_angle = [el*(360 / len(lst_cercle[i])) for el in range(len(lst_cercle[i]))]
-    #         if i != 0:
-    #             lst_angle = [el+(360/len(lst_cercle[i])/2) for el in lst_angle]
-    #         lst_cercle[i][j].x = obj.x + 100*(1+i) * math.cos(lst_angle[j]*math.pi / 180)
-    #         lst_cercle[i][j].y = obj.y + 100*(1+i) * math.sin(lst_angle[j]*math.pi / 180)
-    #         lst_cercle[i][j].affichage_noeud()
-    # obj.affichage_noeud()
     
 
 
 
-graphe(recuperation_page('Catégorie:Histoire'))
+graphe(recuperation_page('Or'))
 
 
 
@@ -130,26 +123,3 @@ Bouton_Quitter.pack()
 tk.mainloop()
 
 
-
-
-
-
-# # ajout du texte à l'intérieur du rectangle
-# text = "Ceci est une bulle de carte mentale"
-# x, y = 0, 0 # coordonnées du centre du rectangle
-# padding = 10 # espace de rembourrage entre le texte et le bord du rectangle
-# bbox = canvas.bbox(canvas.create_text(x, y, text=text)) # dimensions du rectangle englobant
-# bbox = (bbox[0]-padding, bbox[1]-padding, bbox[2]+padding, bbox[3]+padding) # ajout de rembourrage
-
-
-# # dessin du rectangle avec des bords arrondis autour du texte
-# radius = 20 # rayon des coins arrondis
-# rect = create_rounded_rectangle(canvas, *bbox, radius=radius, fill="#3CA6A6", outline="#026773", width=2)
-
-# # création d'une police de caractères en gras
-
-# # ajout du texte centré à l'intérieur du rectangle
-# canvas.create_text(x, y, text=text, width=bbox[2]-bbox[0]-2*padding, justify=tk.CENTER, fill="#FFFFFF")
-
-# # lancement de la boucle principale Tkinter
-# root.mainloop()
