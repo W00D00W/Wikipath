361c9430fee1a4a66e3764bf7926c46c8c628bb1
esteban <esteb.garnil@gmail.com>
2023-04-16 08:42:29 +0200

    barre_menu

--- pageconn.py
+++ pageconn.py
@@ -14,20 +14,23 @@ class page_conn:
         obj.efface()
         obj.page_actuelle = 2
 
+        obj.menu()
+        obj.definition_taille()
+
         self.title_label = customtkinter.CTkLabel(self.tk, text="Espace membre : bienvenue "+user)
-        self.title_label.grid(row=0, column=0, padx=30, pady=15)
+        self.title_label.grid(row=1, column=0, padx=30, pady=15)
 
         text = "Vous n'avez aucune page en favori"
         if len(chercher_captures(self.user)) > 0:
             text = "Pages en favori"
         self.recherche_page = customtkinter.CTkLabel(self.tk, text=text)
-        self.recherche_page.grid(row=1, column=0, padx=0, pady=50)
+        self.recherche_page.grid(row=2, column=0, padx=0, pady=50)
 
         if len(chercher_captures(self.user)) > 0:
             scrollable_frame = customtkinter.CTkScrollableFrame(self.tk, label_text="CTkScrollableFrame")
             scrollable_frame.grid(row=2, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
             scrollable_frame_switches = []
-            cpt = 0
+            cpt = 2
             for element in chercher_captures(self.user):
                 switch = customtkinter.CTkButton(master=scrollable_frame, text=element)
                 switch.grid(row=cpt, column=0, padx=10, pady=(0, 20))
