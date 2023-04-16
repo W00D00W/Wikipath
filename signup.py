a814a443be8e5d373abcdf336d68fbdd65d45590
W00D00W <129957051+W00D00W@users.noreply.github.com>
2023-04-16 11:18:36 +0200

    bdd

--- signup.py
+++ signup.py
@@ -44,7 +44,7 @@ class conn:
         self.login_button.pack(side="left", padx=10, pady=20)
         self.signup_button = customtkinter.CTkButton(self.button_frame, text="S'inscrire", command=lambda : sign_up(self.id_entry.get(), self.mdp_entry.get()))
         self.signup_button.pack(side="left", padx=10, pady=20)
-        self.oublie_button = customtkinter.CTkButton(self.button_frame, text="Mdp oublié ?", command=lambda : sign_up(self.id_entry.get(), self.mdp_entry.get()))
+        self.oublie_button = customtkinter.CTkButton(self.button_frame, text="Mdp oublié ?", command=lambda : avant_mdp_oublie(self.id_entry.get(), obj))
         self.oublie_button.pack(side="left", padx=10, pady=20)
 
             
